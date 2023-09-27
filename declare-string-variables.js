a195d8bfba8b5bf6852917cc82c9dbbb3f243bfc
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-27 22:49:15 +0300

    Create declare-string-variables.js

---
+++ declare-string-variables.js
@@ -0,0 +1,7 @@
+// Tests
+// Waiting:myFirstName should be a string with at least one character in it.
+// Waiting:myLastName should be a string with at least one character in it.
+
+// Solution
+var myFirstName = 'Nikita';
+var myLastName = 'Chistyakov';
