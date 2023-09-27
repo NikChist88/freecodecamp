1f02cef8eee1e3520cbcb1b44c09f82ec4ea3e9d
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-27 22:29:01 +0300

    comment-js-code

---
+++ comment-your-javascript-code.js
@@ -0,0 +1,8 @@
+// Tests
+// Waiting:You should create a // style comment that contains at least five letters.
+// Waiting:You should create a /* */ style comment that contains at least five letters.
+
+// Solution
+// This is an in-line comment.
+/* This is a
+multi-line comment */
