c27015fbaf64c500f74c0cd97ddebaa3b7dc043d
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-27 23:02:18 +0300

    Create understanding-uninitialized-variables.js

---
+++ understanding-uninitialized-variables.js
@@ -0,0 +1,18 @@
+// Tests
+// Waiting:a should be defined and have a final value of 6.
+// Waiting:b should be defined and have a final value of 15.
+// Waiting:c should not contain undefined and should have a final value of the string I am a String!
+// Waiting:You should not change code below the specified comment.
+
+// Solution
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a"
+a = a + 1;
+b = b + 5;
+c = c + " String!";
