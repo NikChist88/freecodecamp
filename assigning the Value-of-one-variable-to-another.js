86411029616b37056b0fa48fda02e7ac4fc76c8e
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-27 22:42:47 +0300

    Create assigning the Value-of-one-variable-to-another.js

---
+++ assigning the Value-of-one-variable-to-another.js
@@ -0,0 +1,13 @@
+// Tests
+// Waiting:You should not change code above the specified comment.
+// Waiting:b should have a value of 7.
+// Waiting:a should be assigned to b with =.
+
+// Solution
+// Setup
+var a;
+a = 7;
+var b;
+b = 7;
+b = a;
+// Only change code below this line
