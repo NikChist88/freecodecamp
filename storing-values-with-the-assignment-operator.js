c28b9e4afd4a4c098fdd330f57224a689bbe87a3
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-27 22:37:02 +0300

    Create storing-values-with-the-assignment-operator.js

---
+++ storing-values-with-the-assignment-operator.js
@@ -0,0 +1,9 @@
+// Tests
+// Waiting:You should not change code above the specified comment.
+// Waiting:a should have a value of 7.
+
+// Solution
+// Setup
+var a;
+a = 7;
+// Only change code below this line
