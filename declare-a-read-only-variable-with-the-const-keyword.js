9898a1e6cbb64c13bf84c380f7f178037f8ed61e
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-29 09:53:06 +0300

    Create declare-a-read-only-variable-with-the-const-keyword.js

---
+++ declare-a-read-only-variable-with-the-const-keyword.js
@@ -0,0 +1,13 @@
+// Tests
+// Waiting:var should not exist in your code.
+// Waiting:You should change fCC to all uppercase.
+// Waiting:FCC should be a constant variable declared with const.
+// Waiting:The string assigned to FCC should not be changed.
+// Waiting:fact should be declared with let.
+// Waiting:console.log should be changed to print the FCC and fact variables.
+
+// Solution
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
