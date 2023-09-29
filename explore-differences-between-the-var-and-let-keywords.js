d682be3db4ccef4ae6f4edb973e47d03f527fa73
NikChist88 <NikChistyakov88@yandex.ru>
2023-09-29 09:45:32 +0300

    Create explore-differences-between-the-var-and-let-keywords.js

---
+++ explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,8 @@
+// Tests
+// Waiting:var should not exist in the code.
+// Waiting:catName should be the string Oliver.
+// Waiting:catSound should be the string Meow!
+
+// Solution
+let catName = "Oliver";
+let catSound = "Meow!";
